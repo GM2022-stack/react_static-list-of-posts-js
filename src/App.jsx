f9627cc0b1678141933afbf138bef971c33dade8
GM2022-stack <guilhermemls20@gmail.com>
2025-09-09 16:42:52 -0300

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,16 @@ import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList/PostList';
 
-function getUserById(userId) {
-  return usersFromServer.find(user => user.id === userId) || null;
-}
-
-function getCommentsByPostId(postId) {
-  return commentsFromServer.filter(comment => comment.postId === postId);
-}
-
-export const posts = postsFromServer.map(post => ({
+const posts = postsFromServer.map(post => ({
   ...post,
-  user: getUserById(post.userId),
-  comments: getCommentsByPostId(post.id),
+  user: usersFromServer.find(user => user.id === post.userId),
+  comments: commentsFromServer.filter(comment => comment.postId === post.id),
 }));
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
+
     <PostList posts={posts} />
   </section>
 );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,24 +1,22 @@
+import './PostInfo.scss';
 import { CommentList } from '../CommentList/CommentList';
 import { UserInfo } from '../UserInfo/UserInfo';
-import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => {
-  const { title, body, user, comments } = post;
-
-  return (
-    <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{title}</h3>
-
-        <UserInfo user={user} />
-      </div>
-      <p className="PostInfo__body">{body}</p>
-      <hr />
-      {comments.length > 0 ? (
-        <CommentList comments={comments} />
-      ) : (
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      )}
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+      <p>
+        {' Posted by  '}
+        <UserInfo user={post.user} />
+      </p>
     </div>
-  );
-};
+    <p className="PostInfo__body">{post.body}</p>
+
+    {post.comments.length > 0 ? (
+      <CommentList comments={post.comments} />
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
+);
